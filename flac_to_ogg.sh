5332aa894b206c8af089a2d402193d07cad95cff
Christopher M. Fuhrman <cfuhrman@pobox.com>
2022-01-11 21:54:40 -0800

    Convert colons in album names to a dash
    As SMB/CIFS and AFS do not like colons in file names, be sure to convert the
    colon to a dash (minus-sign).

--- flac_to_ogg.sh
+++ flac_to_ogg.sh
@@ -77,7 +77,7 @@ for i in "$src_dir"/*.flac; do
 	TRACKNO=$(ffprobe -loglevel error -show_entries format_tags=track -of default=noprint_wrappers=1:nokey=1 "$i")
 	TITLE=$(ffprobe -loglevel error -show_entries format_tags=title -of default=noprint_wrappers=1:nokey=1 "$i" | sed 's/\//_/g')
 	ALBUM=$(ffprobe -loglevel error -show_entries format_tags=album -of default=noprint_wrappers=1:nokey=1 "$i")
-	OGGFILE=$( printf "%02d - %s - %s.ogg" ${TRACKNO} "${ALBUM}" "${TITLE}" )
+	OGGFILE=$( printf "%02d - %s - %s.ogg" ${TRACKNO} "${ALBUM/:/-}" "${TITLE}" )
 
 	oggenc -q9 -o "${dst_dir}${OGGFILE}" "$i"
 done
